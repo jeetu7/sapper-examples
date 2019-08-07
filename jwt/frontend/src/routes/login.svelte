b5acc80f9aae461ef4ab9e39c0e4a402f530d1dc
jeetu7 <jeetu7@yandex.com>
2019-08-07 08:35:55 +0530

    Minor changes.

--- jwt/frontend/src/routes/login.svelte
+++ jwt/frontend/src/routes/login.svelte
@@ -11,11 +11,11 @@ async function handleLogin(event) {
 <form on:submit|preventDefault="{handleLogin}">
   <div>
     <label for="username">Username</label>
-    <input required type="string" id="username" >
+    <input required type="string" id="username" value="TheDude">
   </div>
   <div>
     <label for="password">Password</label>
-    <input required type="password" id="password" >
+    <input required type="password" id="password" value="abides">
   </div>
   
   <button type="submit">Login</button>
